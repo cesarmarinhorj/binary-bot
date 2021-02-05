0a0a227e85c445d55d23fc35f9d688d2ef3ba3dc
Mauricio Maciel <mbmaciel@gmail.com>
2021-02-05 10:30:41 -0300

    ajuste no logo

--- src/indexPage/react-components/logo.jsx
+++ src/indexPage/react-components/logo.jsx
@@ -4,9 +4,8 @@ import { createUrl } from '../../common/utils/tools';
 const Logo = () => (
     <a href='http://onebotmembers.com' id="logo">
         <div className="logo-parent">
-            <div className="binary-logo-text">
+            
                 <img className="responsive" src={'image/logo_main.png'} alt="Binary logo" />
-            </div>
         </div>
     </a>
 );
